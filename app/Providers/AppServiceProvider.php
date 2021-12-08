06a05c2b50e183e413657ca9807721ecebbc74c6
nobmurakami <mhpdm541@gmail.com>
2021-12-08 11:33:17 +0900

    Make SQL output to the log

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
+use DateTime;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        DB::listen(function ($query) {
+            $sql = $query->sql;
+
+            foreach ($query->bindings as $binding) {
+                if (is_string($binding)) {
+                    $binding = "'{$binding}'";
+                } elseif (is_bool($binding)) {
+                    $binding = $binding ? '1' : '0';
+                } elseif (is_int($binding)) {
+                    $binding = (string) $binding;
+                } elseif ($binding === null) {
+                    $binding = 'NULL';
+                } elseif ($binding instanceof Carbon) {
+                    $binding = "'{$binding->toDateTimeString()}'";
+                } elseif ($binding instanceof DateTime) {
+                    $binding = "'{$binding->format('Y-m-d H:i:s')}'";
+                }
+
+                $sql = preg_replace('/\?/', $binding, $sql, 1);
+            }
+
+            logger()->debug('SQL: '.$sql. ' ['.$query->time.' ms]');
+        });
     }
 }
